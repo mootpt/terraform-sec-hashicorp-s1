1d12479a0fce2168c5953b83a76f6a53ba19c9e0
Jay Wallace <jaycwallace@gmail.com>
2021-10-22 12:48:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 variable "company_name" {
   type        = string
   description = "name of company to lookup s-1 status"
-  default     = "hashicorp"
+  default     = "doordash"
 }
 
 locals {
